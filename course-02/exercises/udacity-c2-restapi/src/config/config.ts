8939e707eb41c2d7f1995b754739d3d715dfb310
Vu Khiem Tinh <ktinh.v@gmail.com>
2022-11-12 15:34:54 +0900

    udacity project 2

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+  "postgress": {
+    "username": "database1",
+    "password": "database1",
+    "database": "database1",
+    "host": "database1.cot3y5yhhl10.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_region": "",
+    "aws_profile": "",
+    "aws_media_bucket": ""
   },
   "jwt": {
     "secret": " "
